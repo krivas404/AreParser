c037a56297b884ad6f3f27013529de2c1963dcec
snaiper04ek353 <snaiper04ek353@gmail.com>
2021-08-30 09:15:54 +0300

    refactor

--- main.py
+++ main.py
@@ -21,11 +21,16 @@ def GetHtml(url, params=''):
     r = requests.get(url, headers=headers, params=params)
     return r
 
+def FindCadList(url, params=''):
+    find = requests.get(url, headers=headers, params=search_params)
+    print(find.status_code)
+    return find
+
 def GetContent(html):
     soup = bs(html, 'html.parser')
     items = soup.find_all('td', class_='brdw1111')
-    real_estate_object = []
-    print(items)
+    real_estate_objects = []
+    print('Items : ', items)
 
     # for item in items:
     #     kek = item.find_all('td', class_='td')
@@ -34,7 +39,7 @@ def GetContent(html):
     #         a.append(asd)
 
     for item in items:
-        real_estate_object.append(
+        real_estate_objects.append(
             {
                 'title': item.find('a').get_text(strip=True),
                 'link' : url + item.find('a').get('href'),
@@ -48,33 +53,28 @@ def Parser():
         num_pages = GetNumPages()
         for page in range(1, num_pages):
             print(f'Парсинг страницы №: {page}')
-            html = GetHtml(url, params='online_request_search_page=7#')
+            html = GetHtml(url, params=page)
     else:
         print('Parser error: ', html.status_code)
-
-def FindCadList(url):
-    f = requests.get(url, headers=headers, params=search_params)
-    print(f.status_code)
-    return f
 
 
 def GetNumPages(html):
     td = html.find('td', {'id': 'js_es1'})
-    onclick = td.get('onclick')
-    # td = soup.find('td', {'id': 'js_es1'})
-    td.split('online_request_search_page=')[1].split('#')[0] # get string and search MAX page number in <td onclick=>
-    print(td)
-
-    return 0
+    pages_num = 1
+    if td:
+        onclick = td.get('onclick')
+        # td = soup.find('td', {'id': 'js_es1'})
+        pages_num = onclick.split('online_request_search_page=')[1].split('#')[0]  # get string and search MAX page number in <td onclick=>
+    return pages_num
 
 
 def Main():
     time1 = tt.Start('1')
     html = FindCadList(url_result)
     tt.End(time1)
-    # GetContent(html.text)
-    print(html.text)
-
+    RealEstateObjects = GetContent(html.text)
+    # print(html.text)
+    print('RealEstateObjects = ', RealEstateObjects)
 
 if __name__ == '__main__':
     Main()
